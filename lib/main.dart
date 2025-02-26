add5e6a2a70512e9c13be38d4cdb67645ae8639a
Abey Joseph <abeyna567@gmail.com>
2025-02-26 13:33:02 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:date/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData.light(),
+    home: HomeScreen(),
+  ));
+}
