245a4b3a53b73fe80456126ee7495a559a04c829
Abey Joseph <abeyna567@gmail.com>
2025-02-26 15:28:44 +0800

    mobile detection added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:date/home_screen.dart';
+import 'package:date/mobile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:html' as html;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,6 +10,11 @@ void main() {
     debugShowCheckedModeBanner: false,
     theme:
         ThemeData.light().copyWith(textTheme: GoogleFonts.poppinsTextTheme()),
-    home: HomeScreen(),
+    home: (isMobile()) ? MobileScreen() : HomeScreen(),
   ));
+}
+
+bool isMobile() {
+  String userAgent = html.window.navigator.userAgent.toLowerCase();
+  return userAgent.contains("mobi"); // Detects most mobile devices
 }
